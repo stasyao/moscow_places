0cd08c61b83201b1990543ea9aa4631cd777a216
stas <garanzha.s.a@gmail.com>
2021-10-17 22:01:46 +0300

    remove model save method reordering

--- places/models.py
+++ places/models.py
@@ -25,11 +25,6 @@ class Place(models.Model):
         verbose_name = 'Локация'
         verbose_name_plural = 'Локации'
 
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
-
     def __str__(self):
         return self.title
 
