7873cd251311caedcc28572469fbd7f5d3225a6c
stas <garanzha.s.a@gmail.com>
2021-10-18 16:21:05 +0300

    change interface of the command to create records from github data

--- places/management/commands/load_places_from_github.py
+++ places/management/commands/load_places_from_github.py
@@ -1,6 +1,8 @@
+from urllib.parse import urlparse
+
 from django.core.management.base import BaseCommand
 
-from places.utils.create_place_entry_from_json import(
+from places.utils.create_place_entry_from_json import (
     create_place_record_from_github_jsons
 )
 
@@ -9,11 +11,10 @@ class Command(BaseCommand):
     help = 'Загрузить локации и фото к ним из json-файлов с GitHub'
 
     def handle(self, *args, **options):
-        owner, repo, folder = options["owner"], options["repo"], options["folder"]
-        url = f'https://api.github.com/repos/{owner}/{repo}/contents/{folder}'
-        create_place_record_from_github_jsons(url)
+        path_to_folder = urlparse(options['github_url']).path
+        owner, repo, *_, folder = list(filter(None, path_to_folder.split('/')))
+        download_url = f'https://api.github.com/repos/{owner}/{repo}/contents/{folder}'
+        create_place_record_from_github_jsons(download_url)
 
     def add_arguments(self, parser):
-        parser.add_argument('owner', metavar='владелец_репозитория')
-        parser.add_argument('repo', metavar='имя_репозитория')
-        parser.add_argument('folder', metavar='директория_с_json_файлами')
+        parser.add_argument('github_url')
